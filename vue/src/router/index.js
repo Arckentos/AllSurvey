309cb33346da2c1e7f0161ede1519bc3755a2968
Arckentos <29122246+Arckentos@users.noreply.github.com>
2022-05-14 15:48:34 +0200

    Login and register pages + redirections

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -4,12 +4,15 @@ import Surveys from '../views/Surveys.vue';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
 import DefaultLayout from '../components/DefaultLayout.vue';
+import AuthLayout from '../components/AuthLayout.vue';
+import store from '../store';
 
 const routes = [
     {
         path: '/',
         redirect: '/dashboard',
         component: DefaultLayout,
+        meta: { requiresAuth: true },
         children: [
             {
                 path: '/dashboard',
@@ -24,14 +27,24 @@ const routes = [
         ]
     },
     {
-        path: '/login',
-        name: 'Login',
-        component: Login
-    },
-    {
-        path: '/register',
-        name: 'Register',
-        component: Register
+        path: '/auth',
+        redirect: '/login',
+        name: 'Auth',
+        component: AuthLayout,
+        meta: { isGuest: true },
+        children: [
+            {
+                path: '/login',
+                name: 'Login',
+                component: Login
+            },
+            {
+                path: '/register',
+                name: 'Register',
+                component: Register
+            },
+        ],
+
     },
 ];
 
@@ -39,5 +52,15 @@ const router = createRouter({
     history: createWebHistory(),
     routes
 });
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth && !store.state.user.token) {
+        next({ name: 'Login' })
+    } else if (store.state.user.token && to.meta.isGuest) {
+        next({ name: 'Dashboard' });
+    } else {
+        next();
+    }
+})
 
 export default router;
